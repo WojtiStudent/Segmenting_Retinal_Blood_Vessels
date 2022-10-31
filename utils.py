78e077f2587d59b732a524bac4ab1b887d379308
Wojciech Spychalski <wspychalski00@gmail.com>
2022-10-31 01:44:37 +0100

    Add Deep Learning attempt and Results Comparation notebook, small fixes/modifications in other notebooks/scripts

--- utils.py
+++ utils.py
@@ -91,4 +91,14 @@ def mark_differences(manual, result):
                     marked[i][j] = YELLOW
 
     return marked.astype('uint8')
+
+
+#https://github.com/qubvel/segmentation_models/blob/master/examples/binary%20segmentation%20(camvid).ipynb
+def denormalize(x):
+    """Scale image to range 0..1 for correct plot"""
+    x_max = np.percentile(x, 98)
+    x_min = np.percentile(x, 2)    
+    x = (x - x_min) / (x_max - x_min)
+    x = x.clip(0, 1)
+    return x
     
